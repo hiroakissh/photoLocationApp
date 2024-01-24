23442bc839d84f2d653ce488c66d66bafe80e70b
hiroakissh <hiroaki.saito1014@gmail.com>
2024-01-24 23:37:52 +0900

    [add] DetailLocationViewの追加

---
+++ PhotoLocationApp/Views/DeteilLocationView/DetailLocationView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailLocationView.swift
+//  PhotoLocationApp
+//
+//  Created by HiroakiSaito on 2024/01/24.
+//
+
+import SwiftUI
+
+struct DetailLocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailLocationView()
+}
