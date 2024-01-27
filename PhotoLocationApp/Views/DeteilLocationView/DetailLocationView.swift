47b699edf4e5413853230a30478905b2a1de5ff0
hiroakissh <hiroaki.saito1014@gmail.com>
2024-01-27 22:49:19 +0900

    [add] MapKitを使用して登録した箇所にButton付きのアノテーションの作成

--- PhotoLocationApp/Views/DeteilLocationView/DetailLocationView.swift
+++ PhotoLocationApp/Views/DeteilLocationView/DetailLocationView.swift
@@ -6,12 +6,48 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct DetailLocationView: View {
     var selectedLocation: SaveLocationSwiftModel
 
     var body: some View {
         Text(selectedLocation.name)
+        // TODO: Mapで位置情報を表示
+        // TODO: Conditionがあれば、それを表示する
+        // TODO: もし画像が表示できることがあれば、表示できるようにするのを考える
+        Map(
+            initialPosition: .region(
+                .init(
+                    center: .init(
+                        latitude: selectedLocation.latitude,
+                        longitude: selectedLocation.longitude
+                    ),
+                    span: .init()
+                )
+            )
+        )
+        {
+            Annotation(
+                coordinate: .init(
+                    latitude: selectedLocation.latitude,
+                    longitude: selectedLocation.longitude
+                )) {
+                    VStack{
+                        Button {
+                            print("Selected Button")
+                        } label: {
+                            Text("view Photo")
+                        }
+                    }
+                    .foregroundColor(.blue)
+                    .padding()
+                    .background(in: .capsule)
+                } label: {
+                    Text(selectedLocation.name)
+                }
+
+        }
     }
 }
 
